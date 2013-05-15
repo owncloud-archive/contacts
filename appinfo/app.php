743ac44298cd30aed5b3d5fa6773b987fccca545
Thomas Tanghus <thomas@tanghus.net>
2013-05-15 23:52:41 +0200

    Map to custom CompoundProperty

--- appinfo/app.php
+++ appinfo/app.php
@@ -4,17 +4,20 @@ namespace OCA\Contacts;
 use \OCA\AppFramework\Core\API;
 
 //require_once __DIR__ . '/../controller/groupcontroller.php';
-\Sabre\VObject\Component::$classMap['VCARD'] = '\OCA\Contacts\VObject\VCard';
-\Sabre\VObject\Property::$classMap['FN'] = '\OC\VObject\StringProperty';
-\Sabre\VObject\Property::$classMap['TITLE'] = '\OC\VObject\StringProperty';
-\Sabre\VObject\Property::$classMap['ROLE'] = '\OC\VObject\StringProperty';
-\Sabre\VObject\Property::$classMap['NOTE'] = '\OC\VObject\StringProperty';
-\Sabre\VObject\Property::$classMap['NICKNAME'] = '\OC\VObject\StringProperty';
-\Sabre\VObject\Property::$classMap['EMAIL'] = '\OC\VObject\StringProperty';
-\Sabre\VObject\Property::$classMap['TEL'] = '\OC\VObject\StringProperty';
-\Sabre\VObject\Property::$classMap['IMPP'] = '\OC\VObject\StringProperty';
-\Sabre\VObject\Property::$classMap['URL'] = '\OC\VObject\StringProperty';
-\Sabre\VObject\Property::$classMap['GEO'] = '\Sabre\VObject\Property\Compound';
+\Sabre\VObject\Component::$classMap['VCARD']	= '\OCA\Contacts\VObject\VCard';
+\Sabre\VObject\Property::$classMap['FN']		= '\OC\VObject\StringProperty';
+\Sabre\VObject\Property::$classMap['TITLE']		= '\OC\VObject\StringProperty';
+\Sabre\VObject\Property::$classMap['ROLE']		= '\OC\VObject\StringProperty';
+\Sabre\VObject\Property::$classMap['NOTE']		= '\OC\VObject\StringProperty';
+\Sabre\VObject\Property::$classMap['NICKNAME']	= '\OC\VObject\StringProperty';
+\Sabre\VObject\Property::$classMap['EMAIL']		= '\OC\VObject\StringProperty';
+\Sabre\VObject\Property::$classMap['TEL']		= '\OC\VObject\StringProperty';
+\Sabre\VObject\Property::$classMap['IMPP']		= '\OC\VObject\StringProperty';
+\Sabre\VObject\Property::$classMap['URL']		= '\OC\VObject\StringProperty';
+\Sabre\VObject\Property::$classMap['N']			= '\OC\VObject\CompoundProperty';
+\Sabre\VObject\Property::$classMap['CATEGORIES'] = '\OC\VObject\CompoundProperty';
+\Sabre\VObject\Property::$classMap['ADR']		= '\OC\VObject\CompoundProperty';
+\Sabre\VObject\Property::$classMap['GEO']		= '\OC\VObject\CompoundProperty';
 
 // dont break owncloud when the appframework is not enabled
 if(\OCP\App::isEnabled('appframework')) {
