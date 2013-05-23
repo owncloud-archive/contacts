53ddbaa9ac6581c4215238188d13e653b8c5da49
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-05-24 00:54:03 +0200

    [tx-robot] updated from transifex

--- l10n/en@pirate.php
+++ l10n/en@pirate.php
@@ -1,6 +1,3 @@
 <?php $TRANSLATIONS = array(
-"Download" => "Download",
-"Upload new photo" => "Upload new portrait",
-"Select photo from ownCloud" => "Select portrait from ownCloud",
 "Title" => "Title"
 );
--- l10n/my_MM.php
+++ l10n/my_MM.php
@@ -1,8 +1,3 @@
 <?php $TRANSLATIONS = array(
-"Cancel" => "ပယ်ဖျက်မည်",
-"Download" => "ဒေါင်းလုတ်",
-"Text" => "စာသား",
-"Title" => "ခေါင်းစဉ်",
-"Add" => "ပေါင်းထည့်",
-"Description" => "ဖော်ပြချက်"
+"Title" => "ခေါင်းစဉ်"
 );
