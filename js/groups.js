e6bfe1d6609e77b6e3870b3f554a4a7a63a7cc66
Thomas Tanghus <thomas@tanghus.net>
2013-05-26 22:12:41 +0200

    Added TODO

--- js/groups.js
+++ js/groups.js
@@ -309,6 +309,8 @@ OC.Contacts = OC.Contacts || {};
 
 	/**
 	* Removes one or more contact ids from a group
+	* TODO: When deleting contacts this method should just remove the contact id
+	* from its internal list without saving.
 	* @param integer|array contactid. An integer id or an array of integer ids.
 	* @param integer groupid. The integer id of the group
 	* @param function cb. Optional call-back function
