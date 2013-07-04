a93ba7e845074da427ef148bb6c180d1ba9eee00
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-07-04 09:40:44 +0200

    [tx-robot] updated from transifex

--- l10n/my_MM.php
+++ l10n/my_MM.php
@@ -1,8 +1,3 @@
 <?php $TRANSLATIONS = array(
-"Cancel" => "ပယ်ဖျက်မည်",
-"Add" => "ပေါင်းထည့်",
-"Download" => "ဒေါင်းလုတ်",
-"Text" => "စာသား",
-"Title" => "ခေါင်းစဉ်",
-"Description" => "ဖော်ပြချက်"
+"Title" => "ခေါင်းစဉ်"
 );
--- l10n/sq.php
+++ l10n/sq.php
@@ -1,26 +1,3 @@
 <?php $TRANSLATIONS = array(
-"No file was uploaded. Unknown error" => "Nuk u ngarkua asnjë skedar. Veprim i gabuar i panjohur",
-"There is no error, the file uploaded with success" => "Nuk pati veprime të gabuara, skedari u ngarkua me sukses",
-"The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form" => "Skedari i ngarkuar tejkalon udhëzimin MAX_FILE_SIZE të specifikuar në formularin HTML",
-"The uploaded file was only partially uploaded" => "Skedari i ngarkuar u ngarkua vetëm pjesërisht",
-"No file was uploaded" => "Nuk u ngarkua asnjë skedar",
-"Missing a temporary folder" => "Një dosje e përkohshme nuk u gjet",
-"Failed to write to disk" => "Ruajtja në disk dështoi",
-"Not enough storage available" => "Nuk ka mbetur hapësirë memorizimi e mjaftueshme",
-"Contacts" => "Kontake",
-"Save" => "Ruaj",
-"Cancel" => "Anulo",
-"Add" => "Shto",
-"Error" => "Veprim i gabuar",
-"Download" => "Shkarko",
-"Edit" => "Ndrysho",
-"Delete" => "Elimino",
-"Work" => "Punë",
-"Text" => "Tekst",
-"Title" => "Titulli",
-"Birthday" => "Ditëlindja",
-"Email" => "Email-i",
-"Address" => "Adresa",
-"Share" => "Nda",
-"Name" => "Emri"
+"Title" => "Titulli"
 );
