d99fca0eb72bc1c401fe5ef79b9e7bfb12fcff1f
Michael Gapczynski <mtgap@owncloud.com>
2013-08-02 11:38:07 -0400

    Integrate contacts with Share API 2

--- lib/share/addressbook.php
+++ lib/share/addressbook.php
@@ -1,115 +1,105 @@
 <?php
 /**
- * Copyright (c) 2012 Bart Visscher <bartv@thisnet.nl>
- * This file is licensed under the Affero General Public License version 3 or
- * later.
- * See the COPYING-README file.
+ * ownCloud
+ *
+ * @author Michael Gapczynski
+ * @copyright 2013 Michael Gapczynski mtgap@owncloud.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace OCA\Contacts\Share;
-use OCA\Contacts\Backend\Database;
 
-class Addressbook implements \OCP\Share_Backend_Collection {
-	const FORMAT_ADDRESSBOOKS = 1;
-	const FORMAT_COLLECTION = 2;
+use OC\Share\Share;
 
-	public $backend;
+/**
+ * Data holder for shared address books
+ */
+class AddressBook extends Share {
+
+	protected $description;
+	protected $lastmodified;
+	protected $uri;
 
 	public function __construct() {
-		// Currently only share
-		$this->backend = new Database();
+		$this->addType('itemSource', 'int')
+		$this->addType('lastmodified', 'int');
 	}
 
 	/**
-	* @brief Get the source of the item to be stored in the database
-	* @param string Item
-	* @param string Owner of the item
-	* @return mixed|array|false Source
-	*
-	* Return an array if the item is file dependent, the array needs two keys: 'item' and 'file'
-	* Return false if the item does not exist for the user
-	*
-	* The formatItems() function will translate the source returned back into the item
-	*/
-	public function isValidSource($itemSource, $uidOwner) {
-		$addressbook = $this->backend->getAddressBook($itemSource);
-		if(!$addressbook || $addressbook['owner'] !== $uidOwner) {
-			return false;
-		}
-		return true;
+	 * Get the description of the address book
+	 * @return string
+	 */
+	public function getDescription() {
+		return $this->description;
 	}
 
 	/**
-	* @brief Get a unique name of the item for the specified user
-	* @param string Item
-	* @param string|false User the item is being shared with
-	* @param array|null List of similar item names already existing as shared items
-	* @return string Target name
-	*
-	* This function needs to verify that the user does not already have an item with this name.
-	* If it does generate a new name e.g. name_#
-	*/
-	public function generateTarget($itemSource, $shareWith, $exclude = null) {
-		$addressbook = $this->backend->getAddressBook($itemSource);
-
-		$user_addressbooks = array();
-
-		foreach($this->backend->getAddressBooksForUser($shareWith) as $user_addressbook) {
-			$user_addressbooks[] = $user_addressbook['displayname'];
-		}
-		$name = $addressbook['displayname'] . '(' . $addressbook['userid'] . ')';
-		$suffix = '';
-		while (in_array($name.$suffix, $user_addressbooks)) {
-			$suffix++;
-		}
-
-		$suffix = $suffix ? ' ' . $suffix : '';
-		return $name.$suffix;
+	 * Set the description of the address book
+	 * @param string $description
+	 */
+	public function setDescription($description) {
+		$this->description = $description;
 	}
 
 	/**
-	* @brief Converts the shared item sources back into the item in the specified format
-	* @param array Shared items
-	* @param int Format
-	* @return ?
-	*
-	* The items array is a 3-dimensional array with the item_source as the first key and the share id as the second key to an array with the share info.
-	* The key/value pairs included in the share info depend on the function originally called:
-	* If called by getItem(s)Shared: id, item_type, item, item_source, share_type, share_with, permissions, stime, file_source
-	* If called by getItem(s)SharedWith: id, item_type, item, item_source, item_target, share_type, share_with, permissions, stime, file_source, file_target
-	* This function allows the backend to control the output of shared items with custom formats.
-	* It is only called through calls to the public getItem(s)Shared(With) functions.
-	*/
-	public function formatItems($items, $format, $parameters = null, $include = false) {
-		//\OCP\Util::writeLog('contacts', __METHOD__
-		//	. ' ' . $include . ' ' . print_r($items, true), \OCP\Util::DEBUG);
-		$addressbooks = array();
-		if ($format === self::FORMAT_ADDRESSBOOKS) {
-			foreach ($items as $item) {
-				//\OCP\Util::writeLog('contacts', __METHOD__.' item_source: ' . $item['item_source'] . ' include: '
-				//	. (int)$include, \OCP\Util::DEBUG);
-				$addressbook = $this->backend->getAddressBook($item['item_source']);
-				if ($addressbook) {
-					$addressbook['displayname'] = $addressbook['displayname'] . ' (' . $addressbook['owner'] . ')';
-					$addressbook['permissions'] = $item['permissions'];
-					$addressbooks[] = $addressbook;
-				}
-			}
-		} elseif ($format === self::FORMAT_COLLECTION) {
-			foreach ($items as $item) {
-			}
-		}
-		return $addressbooks;
+	 * Get the last modified time of the address book
+	 * @return int
+	 */
+	public function getLastmodified() {
+		return $this->lastmodified;
 	}
 
-	public function getChildren($itemSource) {
-		\OCP\Util::writeLog('contacts', __METHOD__.' item_source: ' . $itemSource, \OCP\Util::DEBUG);
-		$contacts = $this->backend->getContacts($itemSource, null, null, true);
-		$children = array();
-		foreach($contacts as $contact) {
-			$children[] = array('source' => $contact['id'], 'target' => $contact['fullname']);
-		}
-		return $children;
+	/**
+	 * Set the last modified time of the address book
+	 * @param int $lastmodified
+	 */
+	public function setLastmodified($lastmodified) {
+		$this->lastmodified = $lastmodified;
+	}
+
+	/**
+	 * Get the uri of the address book
+	 * @return string
+	 */
+	public function getUri() {
+		return $this->uri;
+	}
+
+	/**
+	 * Set the uri of the address book
+	 * @param string $uri
+	 */
+	public function setUri($uri) {
+		$this->uri = $uri;
+	}
+
+	/**
+	 * Get the address book in the form of an array as expected by the contacts backend
+	 * @return array
+	 */
+	public function toAddressBook() {
+		return array(
+			'id' => $this->getItemSource(),
+			'displayname' => $this->getItemTarget(),
+			'description' => $this->getDescription(),
+			'owner' => $this->getItemOwner(),
+			'lastmodified' => $this->getLastmodified(),
+			'uri' => $this->getUri(),
+			'permissions' => $this->getPermissions(),
+			'backend' => 'shared',
+		);
 	}
 
 }
