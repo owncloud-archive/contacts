68fb85a66e5befdfb354c67acc6dbe53ef1b205f
Thomas Tanghus <thomas@tanghus.net>
2014-02-26 12:30:38 +0100

    Improve search. Fix #385 and #421

--- lib/searchprovider.php
+++ lib/searchprovider.php
@@ -15,13 +15,19 @@ class SearchProvider extends \OC_Search_Provider{
 		foreach($results as $result) {
 			$link = \OCP\Util::linkToRoute('contacts_index').'#' . $result['id'];
 			$props = array();
+			$display = (isset($result['FN']) && $result['FN']) ? $result['FN'] : null;
 			foreach(array('EMAIL', 'NICKNAME', 'ORG') as $searchvar) {
-				if(isset($result[$searchvar]) && count($result[$searchvar]) > 0 && strlen($result[$searchvar][0]) > 3) {
-					$props = array_merge($props, $result[$searchvar]);
+				if(isset($result[$searchvar]) && $result[$searchvar]) {
+					if(is_array($result[$searchvar])) {
+						$result[$searchvar] = array_filter($result[$searchvar]);
+					}
+					$prop = is_array($result[$searchvar]) ? implode(',', $result[$searchvar]) : $result[$searchvar];
+					$props[] = $prop;
+					$display = $display ?: $result[$searchvar];
 				}
 			}
 			$props = array_map($unescape, $props);
-			$searchresults[]=new \OC_Search_Result($result['FN'], implode(', ', $props), $link, (string)$l->t('Contact'));//$name,$text,$link,$type
+			$searchresults[]=new \OC_Search_Result($display, implode(', ', $props), $link, (string)$l->t('Contact'), null);//$name,$text,$link,$type
 		}
 		return $searchresults;
 	}
