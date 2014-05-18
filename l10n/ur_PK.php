06539a842325523eef2f9dc866d10ebfc27274fd
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-05-18 00:48:28 -0400

    [tx-robot] updated from transifex

--- l10n/ur_PK.php
+++ l10n/ur_PK.php
@@ -1,9 +1,13 @@
 <?php $TRANSLATIONS = array(
 "Ok" => "اوکے",
 "Cancel" => "منسوخ کریں",
+"Save" => "حفظ",
 "OK" => "اوکے",
 "Add" => "شامل کریں",
+"Close" => "بند ",
+"Unknown error" => "غیر معروف خرابی",
 "Delete" => "حذف کریں",
+"Name" => "اسم",
 "Share" => "تقسیم",
 "Password" => "پاسورڈ"
 );
