824f00f42d0fbc8282f4f872a70073394339ccfb
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-17 00:48:08 -0400

    [tx-robot] updated from transifex

---
+++ l10n/bn_IN.php
@@ -0,0 +1,13 @@
+<?php $TRANSLATIONS = array(
+"Cancel" => "বাতিল করা",
+"Save" => "সেভ",
+"Add" => "যোগ করা",
+"Import" => "ইম্পোর্ট",
+"Close" => "বন্ধ",
+"Delete" => "মুছে ফেলা",
+"Address" => "ঠিকানা",
+"Name" => "নাম",
+"Share" => "শেয়ার",
+"Export" => "এক্সপোর্ট",
+"Edit" => "এডিট"
+);
