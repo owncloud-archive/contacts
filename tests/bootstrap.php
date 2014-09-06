79bc4679de94421803494b1dc54c2a548263de93
LEDfan <tobia@ledfan.be>
2014-09-06 17:27:13 +0200

    Connect pre_deleteContact when unit tests are run

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,8 +6,7 @@ $RUNTIME_NOAPPS = true;
 if(!defined('PHPUNIT_RUN')) {
 	define('PHPUNIT_RUN', 1);
 }
-
-require_once __DIR__.'/../../core/lib/base.php';
+require_once __DIR__.'/../../../lib/base.php';
 
 if(!class_exists('PHPUnit_Framework_TestCase')) {
 	require_once('PHPUnit/Autoload.php');
@@ -17,3 +16,5 @@ if(!class_exists('PHPUnit_Framework_TestCase')) {
 
 OC_Hook::clear();
 OC_Log::$enabled = true;
+\OCP\Util::connectHook('OCA\Contacts', 'pre_deleteContact', '\OCA\Contacts\Hooks', 'contactDeletion');
+
