a44f4a55bb0cf24a298a809c8fc8f33bb71d235b
Thomas Müller <thomas.mueller@tmit.eu>
2015-07-01 13:12:14 +0200

    Adding yearRange to datePicker - fixes #914

--- js/contacts.js
+++ js/contacts.js
@@ -1335,6 +1335,7 @@ OC.Contacts = OC.Contacts || {};
 			dateFormat : datepickerFormatDate,
 			changeMonth: true,
 			changeYear: true,
+			yearRange: '-100:+0',
 			minDate : new Date(1900,1,1),
 			maxDate : new Date()
 		});
