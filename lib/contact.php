19b7cef3112e62449da0c04fe20b6cf849d68905
Gerald Vogt <vogt@spamcop.net>
2015-12-01 19:08:04 +0100

    Show correct year of birth when birthday is Dec. 31st.

--- lib/contact.php
+++ lib/contact.php
@@ -806,6 +806,7 @@ class Contact extends VObject\VCard implements IPIMObject {
 				$date
 			);
 			$vEvent->DTSTART['VALUE'] = 'DATE';
+			$vEvent->{'SUMMARY'} = $title . ' (' . $date->format('Y') . ')';
 			$vEvent->add('DTEND');
 			$date->add(new \DateInterval('P1D'));
 			$vEvent->DTEND->setDateTime(
@@ -817,7 +818,6 @@ class Contact extends VObject\VCard implements IPIMObject {
 			$vEvent->DTSTAMP->setDateTime($lm);
 			$vEvent->{'UID'} = $this->UID;
 			$vEvent->{'RRULE'} = 'FREQ=YEARLY';
-			$vEvent->{'SUMMARY'} = $title . ' (' . $date->format('Y') . ')';
 			$vEvent->{'TRANSP'} = 'TRANSPARENT';
 			$appInfo = \OCP\App::getAppInfo('contacts');
 			$appVersion = \OCP\App::getAppVersion('contacts');
